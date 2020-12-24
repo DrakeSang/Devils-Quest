bef464ee984b55936b74f01cbfff98dbaca5d21b
DrakeSang <dahak4eto@gmail.com>
2020-12-24 20:00:43 +0200

    Added liquibase configuration, removed initial sql script

---
+++ app/src/main/java/com/DevilsQuest/app/config/liquibase/LiquiBaseConfig.java
@@ -0,0 +1,56 @@
+package com.DevilsQuest.app.config.liquibase;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class LiquiBaseConfig {
+    @Value("${spring.datasource.driverClassName}")
+    private String className;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.liquibase.change-log}")
+    private String changeLogPath;
+    
+    @Bean
+    public SpringLiquibase liquibase()
+    {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(changeLogPath);
+        liquibase.setDataSource(dataSource());
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate()
+    {
+        return new JdbcTemplate(dataSource());
+    }
+
+    public DataSource dataSource()
+    {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(className);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+}
